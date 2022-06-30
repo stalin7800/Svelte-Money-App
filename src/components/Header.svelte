858167f2b7866f6040333fd090d321f273721471
karate7800 <karate7800@gmail.com>
2022-06-30 16:18:42 -0400

    add basic footers and headers

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>View Transactions</li>
+        <li>Add Transactions</li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        border: 1px solid black;
+        border-radius: 5px;
+        position: sticky;
+        top: 0;
+    }
+    ul {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        list-style: none;
+        
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+import Footer from "../components/Footer.svelte";
+import Header from "../components/Header.svelte";
+
+
+</script>
+
+<Header />
+
+<slot> </slot>
+
+<Footer />
